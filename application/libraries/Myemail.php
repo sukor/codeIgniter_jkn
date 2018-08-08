0dc93998749319c56f37699670dcff9c4347232c
Sukor Muhammad <sukor.muhammad@gmail.com>
2018-08-08 09:51:32 +0800

    email

---
+++ application/libraries/Myemail.php
@@ -0,0 +1,60 @@
+<?php
+
+Myemail class{
+
+
+public function __construct(){
+
+		$this->obj=&get_instace();
+
+	}
+
+//sendemailjpn
+	function sendemailjpn(){
+
+		$ci=$this->obj;
+
+
+		
+
+if($CI->config->item('smtp_on')){
+		$config['smtp_host']=$CI->config->item('smtp_host');
+		$config['smtp_user']=$CI->config->item('smtp_user');
+		$config['smtp_pass']=$CI->config->item('smtp_pass');
+		$config['smtp_port']=$CI->config->item('smtp_port');
+		$config['protocol']=$CI->config->item('protocol');
+		$config['starttls']=FALSE;
+		$config['mailtype']="html";
+		$config['charset']='iso-8859-1';
+		$CI->email->initialize($config);
+		foreach ($data as $r) {
+			
+			$CI->email->clear();
+			$CI->email->set_newline("\r\n");
+			$CI->email->from($r['from'],$r['from_name']);
+			$CI->email->to($r['to']);
+			$CI->email->cc($r['cc']);
+			$CI->email->subject($r['subject']);
+			$CI->email->message($r['message']);
+			if($CI->email->send()){
+				return true;
+			}else{
+				show_error($CI->email->print_debugger());
+				return false;
+			}
+		}
+	
+	}	
+
+
+
+
+	}
+
+//end of sendemaljpn
+
+
+}
+
+
+?>
